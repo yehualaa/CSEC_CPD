ae9e9dd3c06dfbfc90108106f43fbe9b5a87ee41
Yehualeshet <167024969+yehualaa@users.noreply.github.com>
2026-02-25 13:48:52 +0300

    Refactor input handling and count implemented teams

--- Team.py
+++ Team.py
@@ -1,16 +1,7 @@
-n = int(raw_input())
-
-
-total_implemented = 0
-
-
+n=int(input())
+c=0
 for i in range(n):
-   
-    sure_list = map(int, raw_input().split())
-    
- 
-    if sum(sure_list) >= 2:
-        total_implemented += 1
-
-
-print total_implemented
+    l=list(map(int,input().split()))
+    if sum(l)>1:
+        c+=1
+print(c)
